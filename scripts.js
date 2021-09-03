62d6c53f0ebce7385bbd99d95d67906f75561049
ndtvinh13 <54735819+ndtvinh13@users.noreply.github.com>
2021-09-03 12:06:21 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const menuIcon = document.querySelector(".buger-menu");
+const navigation = document.querySelector(".navigation");
+menuIcon.addEventListener("click", function () {
+  navigation.classList.toggle("change");
+});
